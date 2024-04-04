dba449e1a3685afb6d07160c1271cdd0b78f83bd
Kyrua <ang02aezs@gmail.com>
2024-04-04 15:30:22 -0600

    muestra pull request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <h1 className="bg-red-200">
-      Hola que tal
+      
+      Prueba de cambio para pull request
     </h1>
   );
 }
